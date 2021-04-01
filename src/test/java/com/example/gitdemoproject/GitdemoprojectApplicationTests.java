9c883dd589fdcb32d112bb166708c029127898c1
SP <devops.cloud.sp@gmail.com>
2021-04-01 15:46:56 -0500

    Initial Commit

---
+++ src/test/java/com/example/gitdemoproject/GitdemoprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gitdemoproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitdemoprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
